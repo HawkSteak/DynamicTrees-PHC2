65eaf39aaa2bb647c4385220061adae02ae0e789
supermassimo <massimocantugarcia@gmail.com>
2022-01-17 00:21:44 -0300

    Test commit
    commit to test discord webhook

---
+++ src/main/java/maxhyper/dtphc2/blocks/MapleSpileBlock.java
@@ -0,0 +1,11 @@
+package maxhyper.dtphc2.blocks;
+
+import net.minecraft.block.Block;
+
+public class MapleSpileBlock extends Block {
+
+    public MapleSpileBlock(Properties properties) {
+        super(properties);
+    }
+
+}
---
+++ src/main/java/maxhyper/dtphc2/items/RipePeppercornItem.java
@@ -0,0 +1,11 @@
+package maxhyper.dtphc2.items;
+
+import net.minecraft.item.Item;
+
+public class RipePeppercornItem extends Item {
+
+    public RipePeppercornItem(Properties properties) {
+        super(properties);
+    }
+
+}
