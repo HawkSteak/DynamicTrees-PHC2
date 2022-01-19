2aaea63210bb4311cf5e1b610ce74c77a56cf267
supermassimo <massimocantugarcia@gmail.com>
2022-01-19 01:55:06 -0300

    removed PHC tree worldgen

--- src/main/java/maxhyper/dtphc2/DynamicTreesPHC2.java
+++ src/main/java/maxhyper/dtphc2/DynamicTreesPHC2.java
@@ -1,6 +1,8 @@
 package maxhyper.dtphc2;
 
 import com.ferreusveritas.dynamictrees.api.registry.RegistryHandler;
+import com.ferreusveritas.dynamictrees.init.DTConfigs;
+import com.pam.pamhc2trees.config.EnableConfig;
 import maxhyper.dtphc2.init.DTPHC2Client;
 import maxhyper.dtphc2.init.DTPHC2Registries;
 import net.minecraft.util.ResourceLocation;
@@ -27,12 +29,57 @@ public class DynamicTreesPHC2
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
-//        if (DTConfigs.WORLD_GEN.get()){
-//            BYG.ENABLE_OVERWORLD_TREES = false;
-//            if (ModList.get().isLoaded("dynamictreesplus")) {
-//                BYG.ENABLE_CACTI = false;
-//            }
-//        }
+        if (DTConfigs.WORLD_GEN.get()){
+            EnableConfig.apple_worldgen.set(false);
+            EnableConfig.avocado_worldgen.set(false);
+            EnableConfig.candlenut_worldgen.set(false);
+            EnableConfig.cherry_worldgen.set(false);
+            EnableConfig.chestnut_worldgen.set(false);
+            EnableConfig.gooseberry_worldgen.set(false);
+            EnableConfig.lemon_worldgen.set(false);
+            EnableConfig.nutmeg_worldgen.set(false);
+            EnableConfig.orange_worldgen.set(false);
+            EnableConfig.peach_worldgen.set(false);
+            EnableConfig.pear_worldgen.set(false);
+            EnableConfig.plum_worldgen.set(false);
+            EnableConfig.walnut_worldgen.set(false);
+            EnableConfig.spiderweb_worldgen.set(false);
+            EnableConfig.hazelnut_worldgen.set(false);
+            EnableConfig.pawpaw_worldgen.set(false);
+            EnableConfig.soursop_worldgen.set(false);
+            EnableConfig.almond_worldgen.set(false);
+            EnableConfig.apricot_worldgen.set(false);
+            EnableConfig.banana_worldgen.set(false);
+            EnableConfig.cashew_worldgen.set(false);
+            EnableConfig.cinnamon_worldgen.set(false);
+            EnableConfig.coconut_worldgen.set(false);
+            EnableConfig.date_worldgen.set(false);
+            EnableConfig.dragonfruit_worldgen.set(false);
+            EnableConfig.durian_worldgen.set(false);
+            EnableConfig.fig_worldgen.set(false);
+            EnableConfig.grapefruit_worldgen.set(false);
+            EnableConfig.lime_worldgen.set(false);
+            EnableConfig.mango_worldgen.set(false);
+            EnableConfig.olive_worldgen.set(false);
+            EnableConfig.papaya_worldgen.set(false);
+            EnableConfig.paperbark_worldgen.set(false);
+            EnableConfig.pecan_worldgen.set(false);
+            EnableConfig.peppercorn_worldgen.set(false);
+            EnableConfig.persimmon_worldgen.set(false);
+            EnableConfig.pistachio_worldgen.set(false);
+            EnableConfig.pomegranate_worldgen.set(false);
+            EnableConfig.starfruit_worldgen.set(false);
+            EnableConfig.vanillabean_worldgen.set(false);
+            EnableConfig.breadfruit_worldgen.set(false);
+            EnableConfig.guava_worldgen.set(false);
+            EnableConfig.jackfruit_worldgen.set(false);
+            EnableConfig.lychee_worldgen.set(false);
+            EnableConfig.passionfruit_worldgen.set(false);
+            EnableConfig.rambutan_worldgen.set(false);
+            EnableConfig.tamarind_worldgen.set(false);
+            EnableConfig.maple_worldgen.set(false);
+            EnableConfig.pinenut_worldgen.set(false);
+        }
     }
 
     private void clientSetup(final FMLClientSetupEvent event) {
